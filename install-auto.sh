134b672752f5b228c1e3faab9f9d339d327fd010
Maxymou <62802655+Maxymou@users.noreply.github.com>
2025-03-31 22:40:47 +0200

    Add files via upload

--- install-auto.sh
+++ install-auto.sh
@@ -434,23 +434,23 @@ install_php() {
     
     # Installation des extensions PHP essentielles
     sudo apt install -y \
-        php8.1 \
-        php8.1-cli \
-        php8.1-common \
-        php8.1-mysql \
-        php8.1-zip \
-        php8.1-gd \
-        php8.1-mbstring \
-        php8.1-curl \
-        php8.1-xml \
-        php8.1-bcmath \
-        php8.1-json \
-        php8.1-opcache \
-        php8.1-intl \
-        php8.1-ldap \
-        php8.1-redis \
-        php8.1-imagick \
-        php8.1-fpm
+        php8.2 \
+        php8.2-cli \
+        php8.2-common \
+        php8.2-mysql \
+        php8.2-zip \
+        php8.2-gd \
+        php8.2-mbstring \
+        php8.2-curl \
+        php8.2-xml \
+        php8.2-bcmath \
+        php8.2-json \
+        php8.2-opcache \
+        php8.2-intl \
+        php8.2-ldap \
+        php8.2-redis \
+        php8.2-imagick \
+        php8.2-fpm
     
     check_command "Installation de PHP et ses extensions réussie" "Échec de l'installation de PHP"
     
@@ -727,7 +727,7 @@ uninstall_application() {
     
     # Désinstaller les paquets
     print_message "Désinstallation des paquets..."
-    sudo apt remove -y apache2 php8.1* mysql-server
+    sudo apt remove -y apache2 php8.2* mysql-server
     sudo apt autoremove -y
     print_message "✅ Paquets désinstallés"
     
